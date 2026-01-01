8c59f2d2a808e63463069bc541dd03a7fe74dbd0
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2026-01-01 19:40:38 +0300

    Add files via upload

---
+++ oxo2.c
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char a1[10] = "1", a2[10] = "2", a3[10] = "3", a4[10] = "4", a5[10] = "5", a6[10] = "6", a7[10] = "7", a8[10] = "8", a9[10] = "9";
+    int turn = 1;
+
+    while (1)
+    {
+        
+    
+    
+        printf("\noxo\n");
+        
+        
+        printf("%s|%s|%s\n", a1, a2, a3);
+        printf("-----\n");
+        printf("%s|%s|%s\n", a4, a5, a6);
+        printf("-----\n");
+        printf("%s|%s|%s\n", a7, a8, a9);
+
+        printf("\nwhere to place what?\n(from 1 to 9)\nq to quit\nr to restart\n> ");
+        char num[10] = "";
+        scanf("%9s", num);
+        system("cls");
+        if (strcmp(num, "1") == 0) {
+            if (turn == 1){
+                strcpy(a1, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a1, "o");
+            }
+            
+        }
+        else if (strcmp(num, "2") == 0) {
+            if (turn == 1){
+                strcpy(a2, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a2, "o");
+            }
+        }
+        else if (strcmp(num, "3") == 0) {
+            if (turn == 1){
+                strcpy(a3, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a3, "o");
+            }
+        }
+        else if (strcmp(num, "4") == 0) {
+            if (turn == 1){
+                strcpy(a4, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a4, "o");
+            }
+        }
+        else if (strcmp(num, "5") == 0) {
+            if (turn == 1){
+                strcpy(a5, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a5, "o");
+            }
+        }
+        else if (strcmp(num, "6") == 0) {
+            if (turn == 1){
+                strcpy(a6, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a6, "o");
+            }
+        }
+        else if (strcmp(num, "7") == 0) {
+            if (turn == 1){
+                strcpy(a7, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a7, "o");
+            }
+        }
+        else if (strcmp(num, "8") == 0) {
+            if (turn == 1){
+                strcpy(a8, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a8, "o");
+            }
+        }
+        else if (strcmp(num, "9") == 0) {
+            if (turn == 1){
+                strcpy(a9, "x");
+            }
+            else if (turn == 0)
+            {
+                strcpy(a9, "o");
+            }
+        }
+        else if (strcmp(num, "q")==0){
+            return 0;
+        }
+        else if (strcmp(num, "r")==0){
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 0;
+        }
+        
+        turn = turn + 1;
+
+        if (turn == 2){
+            turn = 0;
+        }
+
+        system("cls");
+        
+
+
+
+
+        if(strcmp(a1, a2) == 0 && strcmp(a2, a3) == 0) {
+            printf("%s won!", a1);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        if(strcmp(a4, a5) == 0 && strcmp(a5, a6) == 0) {
+            printf("%s won!", a4);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        if(strcmp(a7, a8) == 0 && strcmp(a8, a9) == 0) {
+            printf("%s won!", a7);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+
+
+
+        if(strcmp(a1, a4) == 0 && strcmp(a4, a7) == 0) {
+            printf("%s won!", a1);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        if(strcmp(a2, a5) == 0 && strcmp(a5, a8) == 0) {
+            printf("%s won!", a2);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        if(strcmp(a3, a6) == 0 && strcmp(a6, a9) == 0) {
+            printf("%s won!", a3);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+
+
+        
+        if(strcmp(a1, a5) == 0 && strcmp(a5, a9) == 0) {
+            printf("%s won!", a1);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        if(strcmp(a3, a5) == 0 && strcmp(a5, a7) == 0) {
+            printf("%s won!", a3);
+            strcpy(a1, "1");
+            strcpy(a2, "2");
+            strcpy(a3, "3");
+            strcpy(a4, "4");
+            strcpy(a5, "5");
+            strcpy(a6, "6");
+            strcpy(a7, "7");
+            strcpy(a8, "8");
+            strcpy(a9, "9");
+            turn = 1;
+        }
+        
+    }
+
+}
